14ac58c51082f805661b0b5cd523f42915e03805
MinhHung_T4TEK <dominhhung04032003@gmail.com>
2025-10-19 22:51:57 +0700

    Init project

---
+++ src/main/java/iuh/fit/se/ace_store_www/AceStoreWwwApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.ace_store_www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceStoreWwwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceStoreWwwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/se/ace_store_www/AceStoreWwwApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.ace_store_www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceStoreWwwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
